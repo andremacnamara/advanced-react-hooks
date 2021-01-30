e08db6823f67323b649ce72927beb00f4d0f1338
Andre MacNamara <andremacnamara12@gmail.com>
2021-01-30 17:05:14 +0000

    Simple Counter

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,10 +3,37 @@
 
 import * as React from 'react'
 
+// function nameReducer(previousName, newName) {
+//   return newName
+// }
+
+// const initialNameValue = 'Joe'
+
+// function NameInput() {
+//   const [name, setName] = React.useReducer(nameReducer, initialNameValue)
+//   const handleChange = event => setName(event.target.value)
+//   return (
+//     <>
+//       <label>
+//         Name: <input defaultValue={name} onChange={handleChange} />
+//       </label>
+//       <div>You typed: {name}</div>
+//     </>
+//   )
+// }
+
+function countReducer(state, newState) {
+  return newState;
+}
+
+const initialCountValue = 0;
+
+// function countReducer
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  // const [count, setCount] = React.useState(initialCount)
+    const [count, setCount] = React.useReducer(countReducer, initialCountValue);
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
